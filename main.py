0b5c6a5984aaec979aaf37fbe90059e2d0634351
p-geon <nagano@nlab.ci.i.u-tokyo.ac.jp>
2022-06-14 19:50:50 +0900

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import X
 import numpy as np
 
 
@@ -10,24 +11,36 @@ def create_equation(m: int, n: int, lamb: float=0.):
          A ∈ R^mn
          where m < n.
          Notice that f() is a L-smooth function and especially when λ > 0, f() is strongly convex.
+    Solve the problem using seepest descent with some step-size 1/L by changing lambda.
     """
+
     # create assertions to check the input
     assert m < n, "m must be less than n"
     assert lamb >= 0, "lamb must be nonnegative"
 
-    '''create weight matrix and bias vector'''
-    A = np.random.rand(m, n) # A ∈ R^mn
+    '''create weight matrix and bias vector
+      A ∈ R^mn
+      b ∈ R^m
+      w ∈ R^n
+    '''
+    A = np.random.rand(m, n)
     b = np.random.rand(m)
-    #A = np.ones(shape=[m, n])
-
-    print(A, A.shape)
-    print(b, b.shape)
+    #w = np.random.rand(n)
+    f = lambda x: x*x + 2*x
 
     """
     # calc L2-norm of weight matrix
     l2 = np.linalg.norm(w)
     print("||w||_2^2", l2)
     """
+
+    # create the objective function
+    #f = lambda w: np.linalg.norm(A @ w - b)**2 + lamb * np.linalg.norm(w)**2
+
+    # create the gradient of the objective function
+    #grad_f = lambda w: 2 * A.T @ (A @ w - b) + 2 * lamb * w
+    
+
     
 
 
